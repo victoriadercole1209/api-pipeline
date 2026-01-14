68bed491bc0f6523e8632686b1540b4d97453db2
Maria Victoria Dercole <114238051+victoriadercole1209@users.noreply.github.com>
2026-01-14 15:30:01 -0300

    Update config.py

--- config.py
+++ config.py
@@ -12,8 +12,3 @@ API_BASE_URL = os.getenv('API_BASE_URL','https://iansaura.com/api') # Si no exis
 if not API_TOKEN:
     raise ValueError("API_TOKEN no configurado. Creá un archivo .env")
 
-
-#import os : Este módulo viene con Python. Sirve para: leer variables del sistema, acceder al entorno . Sin os, no se pueden leer variables de entorno.
-#from dotenv import load_dotenv: Esto viene de python-dotenv (que ya se instalo en requirements).Sirve para: “leer el archivo .env y cargarlo en memoria” . Sin esto: Python NO ve el .env
-#load_dotenv() : Busca un archivo .env, Lee sus variables , Las pone disponibles para Python . Después de esto, Python “sabe” que existe API_TOKEN.
-# os.getenv('API_TOKEN') : Esto pregunta “¿Existe una variable de entorno llamada API_TOKEN?” Si existe → devuelve su valor ; Si no → devuelve None
